fb49ad7726518d98bd2db75f5b31a1666ada1530
Huynh Qui Bao <baohuynh6274@gmail.com>
2025-11-14 14:39:23 +0700

    push

--- src/THONGKETOANBO.java
+++ src/THONGKETOANBO.java
@@ -1,3 +1,26 @@
 public class THONGKETOANBO {
-    
+    public class khachhang extends KHACHHANG{
+        public khachhang(){
+            super();
+        }
+        public khachhang(String ho, String ten, int Makh, int SDT){
+            super(ho, ten, Makh, SDT);
+        }
+    }
+    public class hoadon extends HOADON{
+        public hoadon(){
+            super();
+        }
+        public hoadon(int Mahd, int Manv, int Makh, String Ngay, double Tongtien){
+            super(Mahd, Manv, Makh, Ngay, Tongtien);
+        }      
+    }
+    public class chitiethoadon extends CHITIETHOADON{
+        public chitiethoadon(){
+            super();
+        }
+        public chitiethoadon(int Mahd, int Masp, int Makh, int SL, int DG, double Thanhtien){
+            super(Mahd, Masp, Makh, SL, DG, Thanhtien);
+        }
+    }
 }
