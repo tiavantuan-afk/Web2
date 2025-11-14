52ca193c5fe4bfe2e2621904fc6014b67207bd03
Huynh Qui Bao <baohuynh6274@gmail.com>
2025-11-14 14:41:18 +0700

    them

--- src/List_KHACHHANG.java
+++ src/List_KHACHHANG.java
@@ -1,122 +1,131 @@
 import java.util.Scanner;
+
 class List_KHACHHANG {
-    Scanner sc = new Scanner (System.in);
+    Scanner sc = new Scanner(System.in);
     private int n;
     private KHACHHANG[] dskh = new KHACHHANG[100];
-    public void nhap(){
+
+    public void nhap() {
         System.out.print("Nhap n khach hang: ");
         n = sc.nextInt();
-        for(int i=0; i<n; i++){
-            System.out.print("Nhap khach hang thu: "+(i+1));
+        for (int i = 0; i < n; i++) {
+            System.out.print("Nhap khach hang thu: " + (i + 1));
             dskh[i] = new KHACHHANG();
             dskh[i].nhap();
         }
     }
-    public void xuat(){
+
+    public void xuat() {
         System.out.println("Danh sach khach hang: ");
-        for(int i=0; i<n; i++){
+        for (int i = 0; i < n; i++) {
             dskh[i].xuat();
         }
     }
-    public void timkiemtheoma(int macantim){
+
+    public void timkiemtheoma(int macantim) {
         System.out.print("Nhap ma khach hang can tim: ");
         boolean timkiem = false;
-        for (int i=0; i<n; i++){
-            if (dskh[i].getMaKH() == macantim){
+        for (int i = 0; i < n; i++) {
+            if (dskh[i].getMaKH() == macantim) {
                 timkiem = true;
                 dskh[i].xuat();
             }
         }
-        if (!timkiem){
+        if (!timkiem) {
             System.out.println("Khong co trong danh sach.");
         }
     }
-    public void timkiemtheoten(String tencantim){
+
+    public void timkiemtheoten(String tencantim) {
         System.out.print("Nhap ten khach hang: ");
         boolean timkiem = false;
-        for (int i=0; i<n; i++){
-            if(dskh[i].getTen().toLowerCase().equalsIgnoreCase(tencantim.toLowerCase())){
+        for (int i = 0; i < n; i++) {
+            if (dskh[i].getTen().toLowerCase().equalsIgnoreCase(tencantim.toLowerCase())) {
                 timkiem = true;
                 dskh[i].xuat();
             }
         }
-        if (!timkiem){
+        if (!timkiem) {
             System.out.println("Khong co trong danh sach.");
         }
     }
-    public void timkiemtheoho(String hocantim){
+
+    public void timkiemtheoho(String hocantim) {
         System.out.print("Nhap ho can tim: ");
         boolean timkiem = false;
-        for (int i=0; i<n; i++){
-            if (dskh[i].getHo().toLowerCase().equalsIgnoreCase(hocantim.toLowerCase())){
+        for (int i = 0; i < n; i++) {
+            if (dskh[i].getHo().toLowerCase().equalsIgnoreCase(hocantim.toLowerCase())) {
                 timkiem = true;
                 dskh[i].xuat();
             }
         }
-        if (!timkiem){
+        if (!timkiem) {
             System.out.println("Khong co trong danh sach.");
         }
     }
-    public void timkiemtheosdt(int socantim){
+
+    public void timkiemtheosdt(int socantim) {
         System.out.print("Nhap sdt can tim: ");
         boolean timkiem = false;
-        for(int i=0; i<n; i++){
-            if(dskh[i].getSDT() == socantim){
+        for (int i = 0; i < n; i++) {
+            if (dskh[i].getSDT() == socantim) {
                 timkiem = true;
                 dskh[i].xuat();
             }
         }
-        if (!timkiem){
+        if (!timkiem) {
             System.out.println("Khong co trong danh sach.");
         }
     }
-    public void xoakhachhang(int MAKH){
+
+    public void xoakhachhang(int MAKH) {
         boolean timkiem = false;
-        for (int i=0; i<n; i++){
-            if (dskh[i].getMaKH() == MAKH){
-                for (int j=i; j<n; j++){
-                    dskh[i] = dskh[j+1];
+        for (int i = 0; i < n; i++) {
+            if (dskh[i].getMaKH() == MAKH) {
+                for (int j = i; j < n; j++) {
+                    dskh[i] = dskh[j + 1];
                 }
                 dskh[n - 1] = null;
                 n--;
-                System.out.println("Da xoa khach hang co ma: "+MAKH);
+                System.out.println("Da xoa khach hang co ma: " + MAKH);
                 timkiem = true;
                 break;
             }
         }
-        if (!timkiem){
-            System.out.println("Khong tim thay khach hang co ma: "+MAKH);
+        if (!timkiem) {
+            System.out.println("Khong tim thay khach hang co ma: " + MAKH);
         }
     }
-    public void suakhachhang(int MA, KHACHHANG khang){
+
+    public void suakhachhang(int MA, KHACHHANG khang) {
         boolean timkiem = false;
-        for (int i=0; i<n; i++){
-            if(dskh[i].getMaKH() == MA){
+        for (int i = 0; i < n; i++) {
+            if (dskh[i].getMaKH() == MA) {
                 dskh[i] = khang;
-                System.out.println("Da cap nhat thong tin sinh vien: "+MA);
+                System.out.println("Da cap nhat thong tin khach hang: " + MA);
                 timkiem = true;
                 break;
             }
         }
-        if (!timkiem){
+        if (!timkiem) {
             System.out.println("Khong tim thay khach hang de sua: ");
         }
-        
+
     }
-    public void thongkekhachhang(int khthongke){
-            int d=0;
-            System.out.print("Nhap ma khach hang can thong ke: ");
-            khthongke = sc.nextInt();
-            for (int i=0; i<n; i++){
-                if(dskh[i].getMaKH() == khthongke){
-                    d++;
-                }    
+
+    public void thongkekhachhang(int khthongke) {
+        int d = 0;
+        System.out.print("Nhap ma khach hang can thong ke: ");
+        khthongke = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            if (dskh[i].getMaKH() == khthongke) {
+                d++;
             }
-            if (d>=1){
-                System.out.println("Khach hang cu");
-            }
-            else{
-                System.out.println("Khach hang moi");
-            }    
+        }
+        if (d >= 1) {
+            System.out.println("Khach hang cu");
+        } else {
+            System.out.println("Khach hang moi");
+        }
     }
 }
