ddefeb2ffefc71a0de00e3243c30f8e5e360ea46
tiavantuan-afk <tiavantuan@gmail.com>
2025-11-17 23:22:34 +0700

    Change

--- src/Menu_TonKho.java
+++ src/Menu_TonKho.java
@@ -1,3 +1,86 @@
-public class Menu_TonKho {
+import java.util.Scanner;
 
+public class Menu_TonKho {
+    private List_TonKho quanLy;
+    private Scanner sc;
+    private String fileName = "src/data/List_TonKho.txt";
+
+    public Menu_TonKho() {
+        quanLy = new List_TonKho();
+        sc = new Scanner(System.in);
+    }
+
+    public void menu() {
+        int luaChon = -1;
+
+        do {
+            System.out.println("\n===== MENU QUAN LY TON KHO =====");
+            System.out.println("1. Nhap danh sach ton kho");
+            System.out.println("2. Xuat danh sach ton kho");
+            System.out.println("3. Them san pham ton kho");
+            System.out.println("4. Sua thong tin ton kho");
+            System.out.println("5. Xoa san pham (theo ma)");
+            System.out.println("6. Tim kiem (theo ma)");
+            System.out.println("7. Tim kiem (theo ten)");
+            System.out.println("8. Thong ke ton kho");
+            System.out.println("9. Quay lai menu chinh");
+            System.out.println("0. Thoat");
+            System.out.print("Vui long chon: ");
+
+            luaChon = sc.nextInt();
+            sc.nextLine();
+
+            switch (luaChon) {
+                case 1:
+                    quanLy.nhapds();
+                    break;
+                case 2: // ĐỌC FILE MỚI TRƯỚC KHI XUẤT
+                    quanLy.docFile(fileName);
+                    quanLy.xuat();
+                    break;
+                case 3:
+                    quanLy.them();
+                    break;
+                case 4: // ĐỌC FILE TRƯỚC KHI SỬA
+                    quanLy.docFile(fileName);
+                    quanLy.sua();
+                    break;
+                case 5: // ĐỌC FILE TRƯỚC KHI XÓA
+                    quanLy.docFile(fileName);
+                    System.out.print("Nhap ma kho can xoa: ");
+                    String maCanXoa = sc.nextLine();
+                    quanLy.xoaTheoMa(maCanXoa);
+                    break;
+                case 6: // ĐỌC FILE TRƯỚC KHI TÌM
+                    quanLy.docFile(fileName);
+                    System.out.print("Nhap ma kho can tim: ");
+                    String maCanTim = sc.nextLine();
+                    quanLy.timKiemMa(maCanTim);
+                    break;
+                case 7: // ĐỌC FILE TRƯỚC KHI TÌM
+                    quanLy.docFile(fileName);
+                    quanLy.timKiemTen();
+                    break;
+                case 8: // ĐỌC FILE TRƯỚC KHI THỐNG KÊ
+                    quanLy.docFile(fileName);
+                    quanLy.thongKe();
+                    break;
+                case 9:
+                    System.out.println("Quay lai menu chinh...");
+                    return;
+                case 0:
+                    System.out.println("Cam on ban da su dung!");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Lua chon khong hop le!");
+                    break;
+            }
+        } while (luaChon != 0 && luaChon != 9);
+    }
+
+    public static void main(String[] args) {
+        Menu_TonKho menu = new Menu_TonKho();
+        menu.menu();
+    }
 }
